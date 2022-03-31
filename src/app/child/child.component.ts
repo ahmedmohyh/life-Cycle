5e4ea3a105fa2b3d19d37ea0e3e70caa3a5d10de
Ahmed Mousa <Ahmed.Mousa98@eng-st.cu.edu.eg>
2022-03-31 21:01:49 +0200

    setter and content injection have been added

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,19 +1,60 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnChanges , OnInit {
-@Input() title: string = '';
-  constructor() { }
+export class ChildComponent implements OnChanges , OnInit, DoCheck {
+//@Input() title: string = '';
+@Input() set title(name:string){
+  console.log(name);
+this.mytitle = name;
+}
+mytitle:string = '';
+  constructor() {
+    //console.log('the child constructor has been called');
+   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('changes is', changes);
+    //console.log('changes are ', changes);
   }
   ngOnInit(): void {
-    console.log('the child has been initilised');
+    //console.log('the child has been initilised');
+
+  }
+  ngDoCheck(): void {
+    //console.log('changes detected');
+
+  }
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+   // console.log('ng after content init.');
+
+  }
+  ngAfterContentChecked(): void {
+    //Called after every check of the component's or directive's content.
+    //Add 'implements AfterContentChecked' to the class.
+  //  console.log('ng after content checked');
+
+  }
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    //console.log('ng after view init.');
+
+  }
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    //console.log('ng after view checked');
+
+  }
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    console.log('component destroyed.');
 
   }
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+  styleUrls: ['./parent.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class ParentComponent implements OnInit {
   mytitle  = 'title1';
-  constructor() { }
+  constructor() {
+   // console.log('the parent constructor has been called');
+
+/*     setTimeout(() => {
+      this.mytitle = '';
+    }, 3000); */
+  }
 
   ngOnInit(): void {
   }
